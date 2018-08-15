ade2e701084561dadede6f1b43796ef488b81467
abnerli1994 <q82642418@gmail.com>
2018-08-15 14:46:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import Categories from "./components/Categories";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="list">
+      <div>
             <h1>Categories:</h1>
             <Categories />
-        </div>
       </div>
     );
   }
